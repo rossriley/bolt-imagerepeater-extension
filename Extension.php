847201dceb636ea3c8be16caa80695ad2ab04feb
Ross Riley <riley.ross@gmail.com>
2014-08-21 11:49:22 +0100

    first setup

--- Extension.php
+++ Extension.php
@@ -4,12 +4,13 @@ namespace Bolt\Extensions\Imagerepeater;
 
 class Extension extends \Bolt\BaseExtension
 {
-
-    
+  
 
     public function initialize() {
 
-
+        if ($this->app['config']->getWhichEnd()=='backend') {
+            $this->app['twig.loader.filesystem']->prependPath(__DIR__."/twig");
+        }
     }
 
     public function getName()
